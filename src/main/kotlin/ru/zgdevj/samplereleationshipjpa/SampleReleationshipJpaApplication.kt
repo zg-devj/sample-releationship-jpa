8c9856a5af0587f73579e99c3a37e43492a1f9fc
Gleb <webmaster-80@yandex.ru>
2023-12-24 19:41:59 +0300

    feat: init project

---
+++ src/main/kotlin/ru/zgdevj/samplereleationshipjpa/SampleReleationshipJpaApplication.kt
@@ -0,0 +1,11 @@
+package ru.zgdevj.samplereleationshipjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleReleationshipJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleReleationshipJpaApplication>(*args)
+}
---
+++ src/test/kotlin/ru/zgdevj/samplereleationshipjpa/SampleReleationshipJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.zgdevj.samplereleationshipjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleReleationshipJpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
